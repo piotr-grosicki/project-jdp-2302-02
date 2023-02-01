75667b194d8e39f353a059b55ed95924db8b356b
Karol002 <karol.wojcik.002@gmail.com>
2023-02-01 18:31:10 +0100

    Reparing lombok

--- src/main/java/com/kodilla/ecommercee/domain/dto/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/ProductDto.java
@@ -1,32 +1,14 @@
 package com.kodilla.ecommercee.domain.dto;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class ProductDto {
     private Long id;
     private String name;
     private double price;
     private Long groupId;
-
-    public ProductDto(Long id, String name, double price, Long groupId) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.groupId = groupId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public Long getGroupId() {
-        return groupId;
-    }
 }
