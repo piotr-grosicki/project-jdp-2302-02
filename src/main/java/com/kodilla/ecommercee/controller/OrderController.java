49694c27770b4e54de117723cbe6620a188a9b24
KOMP <jakub.cieplowicz@wp.pl>
2023-02-03 10:47:32 +0100

    JDP230202-04

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -1,4 +1,51 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.Order;
+import com.kodilla.ecommercee.domain.dto.OrderDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/ecommercee/orders")
+@RequiredArgsConstructor
 public class OrderController {
+
+
+
+
+    @GetMapping
+    public List<OrderDto> getOrders(){
+        List<OrderDto> orderDtoList = new ArrayList<>();
+        orderDtoList.add(new OrderDto(1L));
+        return orderDtoList;
+
+    }
+
+    @GetMapping(value = "Id}")
+    public OrderDto getOrder(@PathVariable long Id){
+
+        return new OrderDto(1L);
+    }
+
+    @PostMapping
+    public Order addOrder(@RequestBody Order order) {
+        return order;
+    }
+
+    @PutMapping
+    public OrderDto editOrder(@RequestBody OrderDto OrderDto){
+
+        return new OrderDto(1L);
+    }
+
+
+    @DeleteMapping("{Id}")
+    public void deleteOrder(@PathVariable Long Id) {
+
+    }
+
 }
