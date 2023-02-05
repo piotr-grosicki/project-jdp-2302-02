ca35fd7bbfd1821978e25e37d55ee5b4d9733e5f
KOMP <jakub.cieplowicz@wp.pl>
2023-02-05 15:07:12 +0100

    JDP230202-04 dodanie stringa i poprawki w addOrder

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -1,52 +1,47 @@
 package com.kodilla.ecommercee.controller;
 
-import com.kodilla.ecommercee.domain.Order;
+
 import com.kodilla.ecommercee.domain.dto.OrderDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.ArrayList;
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/ecommercee/orders")
 @RequiredArgsConstructor
 public class OrderController {
 
-
-
-
     @GetMapping
     public List<OrderDto> getOrders(){
         List<OrderDto> orderDtoList = new ArrayList<>();
-        orderDtoList.add(new OrderDto(1L));
+        orderDtoList.add(new OrderDto(1L, "zwrot wszytkich orderow"));
         return orderDtoList;
-
     }
 
     @GetMapping(value = "{id}")
     public OrderDto getOrder(@PathVariable long id){
 
-        return new OrderDto(1L);
+        return new OrderDto(2L, "zwrot pojedynczego orderu");
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, value = "{id}")
-    public List<OrderDto> addOrder(@PathVariable Long id,@RequestBody List<OrderDto> orderDtoList) {
+    public List<OrderDto> addOrder(@PathVariable Long id,@RequestBody OrderDto orderDto) {
+        List<OrderDto> orderDtoList= new ArrayList<>();
+        orderDtoList.add(orderDto);
         return orderDtoList;
     }
 
     @PutMapping
     public OrderDto editOrder(@RequestBody OrderDto OrderDto){
 
-        return new OrderDto(1L);
+        return new OrderDto(3L, "test edycji orderu");
     }
-
 
     @DeleteMapping("{id}")
     public void deleteOrder(@PathVariable Long id) {
-
     }
-
 }
