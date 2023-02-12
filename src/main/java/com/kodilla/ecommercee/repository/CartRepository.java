238f6fc404d0b7ae919984f16e135c6e3ad39d40
Jacek Ostapczuk <jacekostapczuk86@gmail.com>
2023-02-12 21:17:38 +0100

    JDP230202-20

--- src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
@@ -6,8 +6,16 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Transactional
 @Repository
 public interface CartRepository   extends CrudRepository<Cart, Long> {
+
+    @Override
+    List<Cart> findAll();
+
+    @Override
+    Optional<Cart> findById(Long id);
 }
