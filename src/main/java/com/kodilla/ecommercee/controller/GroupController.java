a0c65ffe975f5b778d4cd4c7593fd0ccd5010a20
Rafal Smolaga <rafsmo1992@gmail.com>
2023-02-18 22:09:36 +0100

    JDP230202-22

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -1,8 +1,13 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.Group;
 import com.kodilla.ecommercee.domain.dto.GroupDto;
+import com.kodilla.ecommercee.mapper.GroupMapper;
+import com.kodilla.ecommercee.service.GroupService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -12,27 +17,35 @@ import java.util.List;
 @RequestMapping("/ecommercee/groups")
 @RequiredArgsConstructor
 public class GroupController {
+    private final GroupService groupService;
+    private final GroupMapper groupMapper;
 
     @GetMapping
-    public List<GroupDto> getAllGroups() {
-        return new ArrayList<>();
+    public ResponseEntity<List<GroupDto>> getAllGroups() {
+        List<Group> groups = groupService.getAllGroups();
+        return ResponseEntity.ok(groupMapper.mapToGroupDtoList(groups));
+    }
+
+    @GetMapping(value = "{groupId}")
+    public ResponseEntity<GroupDto> getGroup(@PathVariable Long groupId){
+        return new ResponseEntity<>(groupMapper.mapToGroupDto(groupService.getGroup(groupId)), HttpStatus.OK);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void createGroup(@RequestBody GroupDto groupDto) {
-       // return new GroupDto(1L, "Test Name");
+    public ResponseEntity<Void> createGroup(@RequestBody GroupDto groupDto) {
+        Group group = groupMapper.mapToGroup(groupDto);
+        groupService.saveGroup(group);
+        return ResponseEntity.ok().build();
     }
 
-
-    @GetMapping(value = "{groupId}")
-    public GroupDto getGroupByid(@PathVariable Long groupId) {
-        return new GroupDto(1L, "Test Group Name");
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<GroupDto> updateGroup(@RequestBody GroupDto groupDto){
+        return ResponseEntity.ok(groupMapper.mapToGroupDto(groupService.updateGroup(groupMapper.mapToUpdatedGroup(groupDto))));
     }
 
-
-    @PutMapping
-    public GroupDto editOneGroup(@RequestBody GroupDto groupDto) {
-        return new GroupDto(1L, "Edited test Name");
+    @DeleteMapping(value = "{groupId}")
+    public ResponseEntity<Void> deleteGroup(@PathVariable Long groupId) {
+        groupService.deleteGroup(groupId);
+        return ResponseEntity.ok().build();
     }
-
 }
